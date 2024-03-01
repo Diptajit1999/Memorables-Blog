550a69ca4af7fd2693d2566aa4fce72b7325d80e
Diptajit Das <diptajitdas66@gmail.com>
2024-03-01 13:58:38 +0530

    Starting the styling for the forntend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Container,AppBar,Typography,Grow,Grid} from "@mui/material"
+import memorables from "./images/memorables.png"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxidth="lg">
+      <AppBar position='static' color='inherit'>
+        <Typography variant="h2" align="Center">Memorables</Typography>
+        <img src={memorables} alt="memorables" height="260"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems='stretch' spacing="3">
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <Forms/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
