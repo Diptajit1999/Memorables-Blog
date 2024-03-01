f25d8ddfd76e2bac1043dd0b8d95c9f42f0d0bcc
Diptajit Das <diptajitdas66@gmail.com>
2024-03-01 12:25:30 +0530

    Initial Backend Setup Done

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,8 +1,9 @@
 const express=require("express")
 
 const postRouter=express.Router()
-const {getPosts}=require("../controllers/post.controllers.js")
+const {getPosts,postData}=require("../controllers/post.controllers.js")
 postRouter.get("/",getPosts)
+postRouter.post("/",postData)
 
 module.exports={
     postRouter
